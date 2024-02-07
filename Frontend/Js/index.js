1879afb39cac06c4a6f7d31cf777cec46aa573ec
Zeng RuoShui <zwanez@Zengs-MacBook-Air.local>
2024-02-07 11:18:24 +0800

    Fixed index.js, added admin html&js

--- Frontend/Js/index.js
+++ Frontend/Js/index.js
@@ -1,43 +1,50 @@
 var colorDictionary = {
-    "azur" : "background: #2ecbbe" ,
-    "iced" : "background: #c9cdd0" ,
-    "haze" : "background: #707173" ,
-    "soya" : "background: #aea797" ,
-    "moon" : "background: #818a8b" ,
-    "noct" : "background: #6d6e70" ,
-    "kuro" : "background: #b7b9b8" ,
-    "rose" : "background: #a59790" ,
-    "null" : "background: #ffffff"
+  azur: "background: #2ecbbe",
+  iced: "background: #c9cdd0",
+  haze: "background: #707173",
+  soya: "background: #aea797",
+  moon: "background: #818a8b",
+  noct: "background: #6d6e70",
+  kuro: "background: #b7b9b8",
+  rose: "background: #a59790",
+  null: "background: #ffffff",
 };
-
 
 // Function to retrieve all products and update the HTML content for the product list
 function getProductData() {
-    var request = new XMLHttpRequest();
+  var request = new XMLHttpRequest();
 
-    request.open('GET', 'http://localhost:8080/GET products', true);
+  request.open("GET", "http://localhost:8080/GET products", true);
 
-    request.onload = function () {
-        var obj = JSON.parse(request.responseText);
-        console.log(obj);
+  request.onload = function () {
+    var obj = JSON.parse(request.responseText);
+    console.log(obj);
 
-        // Get div elements by id
-        var productLine = document.getElementById("product-line");
+    // Get div elements by id
+    var productLine = document.getElementById("product-line");
 
-        for (var i = 0; i < obj.length; i++) {
-            if (obj[i].category_name === '')
+    for (var i = 0; i < obj.length; i++) {
+      if (obj[i].category_name === "Keyboard") {
+        // Update the content of div elements
+        productLine.innerHTML +=
+          '<li class="card-med"' +
+          colorDictionary[obj[i].variant] +
+          ">" +
+          '<div class="card-image">' +
+          '<img src="' +
+          obj[i].picture +
+          '"/>' +
+          "</div>";
+        '<a href="#">' +
+          "<span>" +
+          obj[i].name +
+          "</span>" +
+          '<span>More Info<span class="material-symbols-outlined">arrow_forward</span></span>' +
+          "</a>" +
+          "</li>";
+      }
+    }
+  };
 
-            // Update the content of div elements
-            productLine.innerHTML +=
-                '<li class="card-med" id="serv-groom"'+ colorDictionary[obj[i].variant] + '>' +
-                '<div class="card-image">' +
-                '<img src="' + obj[i].picture + '"/>' + '</div>'
-                '<a href = "#">' + obj[i].product_description + '</p>' +
-                '<p>Price: ' + obj[i].product_price + '</p>' +
-                '<p>Category: ' + obj[i].category_name + '</p>' +
-                '</div>';
-        }
-    };
-
-    request.send();
+  request.send();
 }
